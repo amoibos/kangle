7869f29cd223e4a7be93e5d53fc44daab4789593
amoibos <amoibos@gmail.com>
2011-05-08 19:09:10 +0200

    cleanups:         adjusted to python code convention(naming)         Kangle is now a python class         removed unused, unnecesary code(files) optimization:         lesser usage of lists(i.e. tasks, map)         file extension detection functionality:         improved usability(resetCounter, resolution) bug fixes:         changed if condition cause(isdir) for better robustness         check for PIL

--- kangle.py
+++ kangle.py
@@ -1,135 +1,146 @@
 #! /usr/bin/env python
 
-#Copyright 2011 Daniel Oelschlegel. All rights reserved.
+# Copyright 2011 Daniel Oelschlegel. All rights reserved.
 #
-#Redistribution and use in source and binary forms, with or without modification, are
-#permitted provided that the following conditions are met:
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
 #
-#   1. Redistributions of source code must retain the above copyright notice, this list of
-#      conditions and the following disclaimer.
+#   1. Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
 #
-#   2. Redistributions in binary form must reproduce the above copyright notice, this list
-#      of conditions and the following disclaimer in the documentation and/or other materials
-#      provided with the distribution.
+#   2. Redistributions in binary form must reproduce the above copyright 
+#   notice, this list of conditions and the following disclaimer in the 
+#   documentation and/or other materials provided with the distribution.
 #
-#THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR IMPLIED
-#WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-#FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
-#CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-#CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-#ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-#NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-#ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+# NO EVENT SHALL Daniel Oelschlegel OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-#The views and conclusions contained in the software and documentation are those of the
-#authors and should not be interpreted as representing official policies, either expressed
-#or implied, of Daniel Oelschlegel.
+# The views and conclusions contained in the software and documentation are
+# those of the authors and should not be interpreted as representing official
+# policies, either expressed or implied, of Daniel Oelschlegel.
 
 
 # this program requires a plugged kindle, at least python 2.* and PIL
-# start this program in a subdirectory that contains picture files or directories
+# start this program in a subdirectory that contains picture files or 
+# directories
 # press [ALT] + [z] for rereading the entries in pictures on a kindle
 
 import sys
 import os
-import Image
+try:
+    import Image
+except ImportError:
+    print "please install PIL"
+    sys.exit(-2)
 
-# version tracker
-version = 0.1
-author = "Daniel Oelschlegel"
-# global variable counter needed for recursive search
-counter = 0
-# resolution of your kindle, required for stretching and splitting
-width = 600
-height = 800
-screen = (width, height)
-# target_dir is the kindle root directory, dummy value
-target_dir = 'D:\\'
+__author__ = "Daniel Oelschlegel"
+__copyright__ = "Copyright 2011, " + __author__
+#__credits__ = ["", ""]
+__license__ = "BSD"
+__version__ = "0.2"
 
-# kindle reads the files in order of timestamp
-def adjustImage((filename, counter)):
-	st = Image.open(filename)
-	# prevents IOError with this type
-	if st.mode == 'P':
-		special = True
-	else:
-		special = False	
-	# resolution of the image, width and height
-	(w, h) = st.size
-	filename = str(counter).rjust(6, '0') + 'a.jpg'
-	# too wide, better splitting in middle and stretching
-	if w > h:
-		# take the second half and resize
-		nd = st.crop((w / 2, 0, w, h)).resize(screen)
-		st = st.crop((0, 0, w / 2, h))
-		second = os.path.join(target_dir, filename)
-		# manga reading style, right to left, save in correct order
-		if not special:
-			nd.save(second, "JPEG")
-		else:
-			nd.convert("RGB").save(second, "JPEG")
-		filename = filename.replace('a.jpg', 'b.jpg')
-	st = st.resize(screen)
-	first = os.path.join(target_dir, filename)
-	if not special:
-		st.save(first, "JPEG")
-	else:
-		st.convert("RGB").save(first, "JPEG")
+class Kangle(object):
+    """Kangle makes manga scans readable on a kindle device."""
+    # needed for recursive search
+    __counter = 0
+    # resolution of your kindle, required for stretching
+    resolution = (600, 800)
+    # target_dir is the kindle root directory, dummy value
+    __target_dir = 'D:\\'
 
-# recursive searching
-def looking(dir_name):
-	dirs = []
-	files = []
-	for item in os.listdir(dir_name):
-		item = os.path.join(dir_name, item)
-		if os.path.isfile(item):
-			files.append(item)
-		else:
-			dirs.append(item)
-	if files:
-		tasks = []
-		# some OSes need this, additional sorting
-		files.sort()
-		for filename in files:
-			global counter
-			name = filename[-4:].lower()
-			# filter for file extensions, 
-			# this must be supported by PIL
-			if name =='.jpg' or \
-				name == '.png' or \
-				name == '.gif':
-				tasks.append((filename, counter))
-				counter += 1
-		map(adjustImage, tasks)
-	
-	# continue with subdirectories
-	for dir in dirs:
-		looking(dir)
+    def resetCounter(self):
+        self.__counter = 0
 
-def main():
-	global target_dir
-	
-	if len(sys.argv) < 2:
-		print "arguments: <TITLE> <KINDLE_ROOT_DIRECTORY>"
-		sys.exit(-1)
-	if sys.argv[1] == "--version":
-		print "Kangle version " + version + " by " + author
-		sys.exit(0)
-	
-	# sys.argv[2] could look like "D:\"(windows) or "/media/kindle"(unix-like)
-	target_dir = sys.argv[2]
-	for dir in ["pictures", sys.argv[1]]:
-		target_dir = os.path.join(target_dir, dir)
-		if not os.path.isdir(target_dir):
-			print "creating directory " + target_dir
-			os.mkdir(target_dir)
-		elif dir == sys.argv[1]:
-			print "directory " + dir + " already exists"
-	print "converting & transferring ...",
-	looking(os.getcwd())
-	print "finished"
+    def run(self, dir):
+        self.resetCounter()
+        print "converting & transferring ...",
+        self.looking(dir)
+        print "finished"
+
+    # kindle reads the files in order of timestamp
+    def adjustImage(self, filename, counter):
+        """Adjust the image file filename to kindle screen and use counter for naming."""
+        first = Image.open(filename)
+        # prevents IOError with this type, convert is often unneeded and slow
+        palleteMode = first.mode == 'P'
+        # resolution of the image
+        (width, height) = first.size
+        filename = '%06da.jpg' % self.__counter
+        # too wide, better splitting in middle and stretching
+        if width > height:
+            # take the second half and resize
+            second = first.crop((width / 2, 0, width, height))
+            second = second.resize(self.resolution)
+            first = first.crop((0, 0, width / 2, height))
+            fullName = os.path.join(self.__target_dir, filename)
+            # manga reading style, right to left, save in correct order
+            if not palleteMode:
+                second.save(fullName, "JPEG")
+            else:
+                second.convert("RGB").save(fullName, "JPEG")
+            filename = filename.replace('a.jpg', 'b.jpg')
+        first = first.resize(self.resolution)
+        fullName = os.path.join(self.__target_dir, filename)
+        if not palleteMode:
+            first.save(fullName, "JPEG")
+        else:
+            first.convert("RGB").save(fullName, "JPEG")
+
+    # optimized recursive search
+    def looking(self, dir_name):
+        """Find supported pictures in dir_name."""
+        dirs = []
+        files = []
+        for item in os.listdir(dir_name):
+            item = os.path.join(dir_name, item)
+            if os.path.isdir(item):
+                dirs.append(item)
+            else:
+                files.append(item)
+        # some OSes need this, additional sorting
+        files.sort()
+        for filename in files:
+            # filter for file extensions, 
+            # this must be supported by PIL
+            if filename[-4:].lower() in ['.jpg', '.png', '.gif']:
+                self.adjustImage(filename, self.__counter)
+                self.__counter += 1
+        
+        # continue with subdirectories
+        for dir in dirs:
+            self.looking(dir)
+
+    def __init__(self, title, target_dir):
+        self.__target_dir = target_dir
+        for dir in ["pictures", title]:
+            self.__target_dir = os.path.join(self.__target_dir, dir)
+            if not os.path.isdir(self.__target_dir):
+                print "creating directory " , self.__target_dir
+                os.mkdir(self.__target_dir)
+            elif dir == sys.argv[1]:
+                print "directory ", dir, " already exists"
 
 if __name__ == "__main__":
-	main()
-	
+    try:
+        # sys.argv[2] could look like "D:\"(windows) or "/media/kindle"(unix)
+        title, target_dir = sys.argv[1], sys.argv[2]
+    except IndexError:
+        if len(sys.argv) < 3 and sys.argv[1] == "--version":
+            print "Kangle version ", __version__," by ", __author__
+            #TODO: credit line
+            sys.exit(0) 
+        else:
+            print "arguments: <TITLE> <KINDLE_ROOT_DIRECTORY>"
+            sys.exit(-1)
+   
+    kangle = Kangle(title, target_dir)    
+    kangle.run(os.getcwd())
+    
