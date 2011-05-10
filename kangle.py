2920f7e5ab0a8ef92618684c4faf0f19f068b740
amoibos <amoibos@gmail.com>
2011-05-10 13:58:07 +0200

    cleanups:         complete refactoring  of the code                 removed duplicated code                 added only used parts to common namespace                 moved class member declaration to __init__                 using os.walk instead of own implementation                 removed resetCounter
    functionality:
            added footnote function
            resume of creating a compilation

--- kangle.py
+++ kangle.py
@@ -33,124 +33,128 @@
 # directories
 # press [ALT] + [z] for rereading the entries in pictures on a kindle
 
-# kindle tip: [ALT] [f] = fullscreen, 2x [ALT] [p] clear the boundary
+# kindle tip: [ALT] + [f] = fullscreen, 2x [ALT] + [p] clear the boundary
 
-import sys
-import os
+from sys import exit, argv
+from os import walk, mkdir, getcwd
+from os.path import join, isdir
+
 try:
-    import Image
+    from PIL import Image, ImageDraw
 except ImportError:
     print "please install PIL"
-    sys.exit(-2)
+    exit(-2)
 
 __author__ = "Daniel Oelschlegel"
 __copyright__ = "Copyright 2011, " + __author__
 __credits__ = [""]
 __license__ = "BSD"
-__version__ = "0.31"
+__version__ = "0.5"
 
 # Kangle, a symbiosis of manga and kindle
 class Kangle(object):
     """Kangle makes manga scans readable on a kindle device."""
-    # reverse order by splitted image, usefull for reading manga
-    # comics should use reverse = False
-    reverse = True
-    # splitting if the image is too wide
-    splitting = True
-    # needed for recursive search
-    _counter = 0
-    # resolution of your kindle, required for stretching
-    resolution = (600, 800)
-    # target_dir is the kindle root directory, dummy value
-    _target_dir = 'D:\\'
-
-    def resetCounter(self):
-        self._counter = 0
-
+    
     def run(self, dir):
-        self.resetCounter()
         print "converting & transferring ...",
         self.looking(dir)
         print "finished"
 
     # kindle reads the files in order of timestamp
     def adjustImage(self, filename, counter):
-        """Adjust the image file filename to kindle screen and use counter for naming."""
+        """Adjust the image file filename to kindle screen and use counter
+        for naming."""
         first = Image.open(filename)
-        # prevents IOError with this type, convert is often unnecessary and slow
-        palleteMode = first.mode == 'P'
         # resolution of the image
         (width, height) = first.size
-        filename = '%06da.jpg' % self._counter
-        # too wide, better splitting in middle and stretching
-        if splitting and width > height:
+        filename = '%06da.jpg' % counter
+        # too wide, better splitting in middle
+        if self.splitting and width > height:
             # take the second half and resize
             second = first.crop((width / 2, 0, width, height))
-            second = second.resize(self.resolution)
             first = first.crop((0, 0, width / 2, height))
-            fullName = os.path.join(self._target_dir, filename)
-            # reverse : manga reading style, right to left, save in correct order
-            if not reverse:
-                # HINT: the first half resizes twice
-                first, second = second, first.resize(self.resolution)
-            if not palleteMode:
-                second.save(fullName, "JPEG")
-            else:
-                second.convert("RGB").save(fullName, "JPEG")
+            # reverse: manga reading style, right to left
+            # save in correct order
+            if not self.reverse:
+                first, second = second, first
+            self.save(second, filename)
             filename = filename.replace('a.jpg', 'b.jpg')
-        first = first.resize(self.resolution)
-        fullName = os.path.join(self._target_dir, filename)
-        if not palleteMode:
-            first.save(fullName, "JPEG")
+        self.save(first, filename)
+        
+    def save(self, image, filename):
+        """Save the image with the enabled options under the filename."""
+        if self.stretching:
+            image = image.resize(self.resolution)
+        if self.footer:
+            self.makeFootnote(image, '%s@%s' % (filename, self.title))
+        fullName = join(self._target_dir, filename)
+        # prevents IOError with this type,convert is often unnecessary and slow
+        if not image.mode in ['P']:
+            image.save(fullName, "JPEG")
         else:
-            first.convert("RGB").save(fullName, "JPEG")
+            image.convert("RGB").save(fullName, "JPEG")
+    
+    def makeFootnote(self, image, text):
+        """Write the filename@Title downright."""
+        draw = ImageDraw.Draw(image)
+        dim = draw.textsize(text)
+        x1, y1 = image.size[0] - dim[0], image.size[1] - dim[1]
+        draw.rectangle((x1,y1, image.size[0], image.size[1]), fill='white')
+        draw.text((x1, y1), text, fill='black')
 
     # optimized recursive search
-    def looking(self, dir_name):
-        """Find supported pictures in dir_name."""
-        dirs = []
-        files = []
-        for item in os.listdir(dir_name):
-            item = os.path.join(dir_name, item)
-            if os.path.isdir(item):
-                dirs.append(item)
-            else:
-                files.append(item)
-        # some OSes need this, additional sorting
-        files.sort()
-        for filename in files:
-            # filter for file extensions, 
-            # this must be supported by PIL
-            if filename[-4:].lower() in ['.jpg', '.png', '.gif']:
-                self.adjustImage(filename, self._counter)
-                self._counter += 1
-        # continue with subdirectories
-        for dir in dirs:
-            self.looking(dir)
-
-    def __init__(self, title, target_dir):
+    def looking(self, dir):
+        """Find supported pictures in dir."""
+        counter = self._counter
+        for curr_dir, dirs, files in walk(dir):
+            dirs.sort()
+            files.sort()
+            for filename in files:
+                # filter for file extensions, 
+                # this must be supported by PIL
+                if filename[-4:].lower() in ['.jpg', '.png', '.gif']:
+                    fullName = join(curr_dir, filename)
+                    self.adjustImage(fullName, counter)
+                    counter += 1  
+        self._counter = counter
+        
+    def __init__(self, title, target_dir, counter=0):
+        # reverse order by splitted image, usefull for reading manga
+        # comics should use reverse = False
+        self.reverse = True
+        # splitting if the image is too wide
+        self.splitting = True
+        # stretching
+        self.stretching = True
+        # footnote displays the name of the image file
+        self.footer = True#False
+        # needed for recursive search
+        self._counter = counter
+        # resolution of your kindle, required for stretching
+        self.resolution = (600, 800)
+        self.title = title
         self._target_dir = target_dir
         for dir in ["pictures", title]:
-            self._target_dir = os.path.join(self._target_dir, dir)
-            if not os.path.isdir(self._target_dir):
+            self._target_dir = join(self._target_dir, dir)
+            if not isdir(self._target_dir):
                 print "creating directory " , self._target_dir
-                os.mkdir(self._target_dir)
-            elif dir == sys.argv[1]:
+                mkdir(self._target_dir)
+            elif dir == argv[1]:
                 print "directory ", dir, " already exists"
 
 if __name__ == "__main__":
     try:
         # sys.argv[2] could look like "D:\"(windows) or "/media/kindle"(unix)
-        title, target_dir = sys.argv[1], sys.argv[2]
+        title, target_dir = argv[1], argv[2]
     except IndexError:
         if len(sys.argv) > 1 and sys.argv[1] == "--version":
             print "Kangle version ", __version__," by ", __author__
             print "Thanks to", __credits__
-            sys.exit(0) 
+            exit(0) 
         else:
             print "arguments: <TITLE> <KINDLE_ROOT_DIRECTORY>"
-            sys.exit(-1)
+            exit(-1)
    
     kangle = Kangle(title, target_dir)    
-    kangle.run(os.getcwd())
+    kangle.run(getcwd())
     
